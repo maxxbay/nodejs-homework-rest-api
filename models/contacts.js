b0ef44cb88b8cbafea2351976b1b3d8b5f86cdc7
Maxxbay <maxxbay@gmail.com>
2022-11-13 17:00:06 +0100

    validation

--- models/contacts.js
+++ models/contacts.js
@@ -34,11 +34,11 @@ const updateFavoriteSchema = Joi.object({
 
 const schemas = {
   addSchema,
+  updateFavoriteSchema,
 };
 const Contact = model("contact", contactSchema);
 
 module.exports = {
   Contact,
   schemas,
-  updateFavoriteSchema,
 };
