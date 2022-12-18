ada126afcceb94e0a066414a9c08d1cbc97fdad9
Maxxbay <maxxbay@gmail.com>
2022-12-18 14:12:18 +0100

    avatars

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,12 @@ const register = require("./register.js");
 const login = require("./login.js");
 const current = require("./getCurrent.js");
 const logout = require("./logout.js");
+const updateAvatar = require("./updateAvatar.js");
 
 module.exports = {
   register,
   login,
   current,
   logout,
+  updateAvatar,
 };
